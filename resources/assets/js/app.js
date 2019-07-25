f744ade2681001e6eee5cb6e6de9d7e4a1d8f119
Cesar Melo <csrmds@gmail.com>
2019-07-25 01:13:12 -0300

    Aula 4 concluida - Criando o Componente Topo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('topo', require('./components/Topo.vue'));
 
 const app = new Vue({
     el: '#app'
