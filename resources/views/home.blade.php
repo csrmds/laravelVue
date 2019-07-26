43717f16b4734ac03dfccf812c0fdb548dbd05de
Cesar Melo <csrmds@gmail.com>
2019-07-26 14:27:47 -0300

    Concluido aula 09 - Componente Pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<painel titulo="Dashboard">
-					<div class="row">
-						<div class="col-md-4">
-							<caixa qtd="199" cor="orange" titulo="Artigos!" icone="ion ion-pie-graph" url="#"></caixa>
-						</div>
-						<div class="col-md-4">
-							<caixa qtd="3000" cor="magenta" titulo="Usuários" icone="ion ion-pie-graph" url="#"></caixa>
-						</div>
-						<div class="col-md-4">
-							<caixa qtd="9" cor="purple" titulo="Autores" icone="ion ion-pie-graph" url="#"></caixa>
-						</div>
-					</div>
-				</painel>
+	<pagina tamanho="x">		
+
+		<painel titulo="Dashboard" >
+			<div class="row">
+				<div class="col-md-4">
+					<caixa qtd="199" cor="orange" titulo="Artigos!" icone="ion ion-pie-graph" url="#"></caixa>
+				</div>
+				<div class="col-md-4">
+					<caixa qtd="3000" cor="magenta" titulo="Usuários" icone="ion ion-pie-graph" url="#"></caixa>
+				</div>
+				<div class="col-md-4">
+					<caixa qtd="9" cor="purple" titulo="Autores" icone="ion ion-pie-graph" url="#"></caixa>
+				</div>
 			</div>
-		</div>
-	</div>
+		</painel>
+
+	</pagina>
+
 @endsection
