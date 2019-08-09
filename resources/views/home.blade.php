a5998edec203143585d71ee0f56ac277c31eac20
Cesar Melo <csrmds@gmail.com>
2019-08-09 14:31:31 -0300

    Concluido aula 49

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+
 	<pagina tamanho="12">		
 
-		<painel titulo="Lista de Artigos" >
+		<painel titulo="Dashboard" >
 			<migalhas v-bind:lista="{{$listaMigalhas}}" ></migalhas>
-
+			
 			<div class="row">
 				<div class="col-md-4">
-					<caixa qtd="199" cor="orange" titulo="Artigos!" icone="ion ion-pie-graph" url="{{route('artigos.index')}}"></caixa>
+					<caixa qtd="{{$totalArtigos}}" cor="orange" titulo="Artigos" icone="ion ion-pie-graph" url="{{route('artigos.index')}}"></caixa>
 				</div>
 				<div class="col-md-4">
-					<caixa qtd="3000" cor="magenta" titulo="Usuários" icone="ion ion-pie-graph" url="{{route('usuarios.index')}}"></caixa>
+					<caixa qtd="{{$totalUsuarios}}" cor="magenta" titulo="Usuários" icone="ion ion-pie-graph" url="{{route('usuarios.index')}}"></caixa>
 				</div>
 				<div class="col-md-4">
-					<caixa qtd="9" cor="purple" titulo="Autores" icone="ion ion-pie-graph" url="{{route('autores.index')}}"></caixa>
+					<caixa qtd="{{$totalAutores}}" cor="purple" titulo="Autores" icone="ion ion-pie-graph" url="{{route('autores.index')}}"></caixa>
 				</div>
 			</div>
 		</painel>
