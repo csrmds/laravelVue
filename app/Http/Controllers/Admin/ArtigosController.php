61e7d881ac33a336d058b3017c8f9691de85ffeb
Cesar Melo <csrmds@gmail.com>
2019-08-12 12:05:57 -0300

    Concluido aula 52

--- app/Http/Controllers/Admin/ArtigosController.php
+++ app/Http/Controllers/Admin/ArtigosController.php
@@ -28,13 +28,15 @@ class ArtigosController extends Controller
 			//$value->user_id= \App\User::find($value->user_id)->name;
 			$value->user_id= $value->user->name;
 			unset($value->user);
-		}*/
+		}
 
 		$listaArtigos= DB::table('artigos')
 			->join('users','users.id','=','artigos.user_id')
 			->select('artigos.id','artigos.titulo','artigos.descricao','users.name','artigos.data')
 			->whereNull('deleted_at')
-			->paginate(5);
+			->paginate(5);*/
+
+		$listaArtigos= Artigo::listaArtigos(5);
 
 		return view('admin.artigos.index',compact('listaMigalhas', 'listaArtigos'));
 	}
